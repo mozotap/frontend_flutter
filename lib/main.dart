8838bdaa955a339bdfab724491db5cc10cb7de62
franklopezmaxi <159826035+franklopezmaxi@users.noreply.github.com>
2024-06-16 23:27:04 -0500

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      title: 'Flutter Demo',
+      title: 'MozoTap',
       theme: ThemeData(
         colorScheme: ColorScheme(
           brightness: Brightness.light,
